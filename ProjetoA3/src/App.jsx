510cf6c2691381bf94b00e6e767154c2684f0e56
Nathan Rodrigues <nathan.contato23@gmail.com>
2025-04-27 20:57:15 -0300

    feat: inicia interface grafica

--- ProjetoA3/src/App.jsx
+++ ProjetoA3/src/App.jsx
@@ -4,32 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">SNC</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">
+        Em manutenção.
+      </button>
+    </div>
+  );
+}
+
+function Livros() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-4">Livros</h1>
+    </div>
+  );
 }
 
 export default App
